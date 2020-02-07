774f3ec71eff995e92d7008d28c359f11f6a254b
Kaleb Fischer <kalebfischer@flywheel.io>
2020-02-07 15:42:49 -0600

    remove create_metadata_dict

--- tests/test_container_export.py
+++ tests/test_container_export.py
@@ -1,7 +1,6 @@
 import pytest
 from pathlib import Path
-from deid_export.container_export import hash_string, load_template_file, quote_numeric_string, create_metadata_dict, \
-    META_WHITELIST_DICT
+from deid_export.container_export import hash_string, load_template_file, quote_numeric_string
 
 DATA_ROOT = Path(__file__).parent/'data'
 
@@ -41,46 +40,3 @@ def test_quote_numeric_string():
     in_str = '1.1.1'
     out_str = quote_numeric_string(in_str)
     assert out_str == '1.1.1'
-
-
-def test_create_metadata_dict():
-    mock_info = {
-        'spam': 2,
-        'eggs': 'yolk'
-    }
-
-    container_type_list = list(META_WHITELIST_DICT.keys())
-    for container_type in container_type_list:
-        mock_metadata = {'info': mock_info, 'id': 'e99ad2d8959a261b56f6162aa2171825a9907a2a'}
-        for key in META_WHITELIST_DICT.get(container_type):
-            mock_metadata[key] = 'Flywheel'
-
-        # test partial info
-        config = {
-            'whitelist': {
-                'info': ['spam'],
-                'metadata': [key for key in mock_metadata.keys() if key != 'info']
-            }
-        }
-        export_dict = {'origin_id': hash_string(mock_metadata.get('id'))}
-        return_dict = {'info': {'spam': 2, 'export': export_dict}}
-        for key, value in mock_metadata.items():
-            if key not in ['info', 'id']:
-                return_dict[key] = value
-        output_dict = create_metadata_dict(mock_metadata, container_type=container_type, container_config=config)
-        assert output_dict == return_dict
-
-        # test all
-        config = {'whitelist': {'info': 'all', 'metadata': 'all'}}
-        return_dict = dict()
-        for key, value in mock_metadata.items():
-            if key not in ['id']:
-                return_dict[key] = value
-        return_dict['info']['export'] = export_dict
-        output_dict = create_metadata_dict(mock_metadata, container_type=container_type, container_config=config)
-        assert output_dict == return_dict
-
-        # test none
-        return_dict = {'info': {'export': export_dict}}
-        output_dict = create_metadata_dict(mock_metadata, container_type=container_type)
-        assert output_dict == return_dict
